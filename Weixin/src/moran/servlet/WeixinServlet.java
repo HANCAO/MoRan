d0d4163251635f63cfc5a95d10f53a9ea53c49fc
韩思琛 <helpsmileycaesar@foxmail.com>
2018-04-01 20:09:38 +0800

    项目主目录

---
+++ Weixin/src/moran/servlet/WeixinServlet.java
@@ -0,0 +1,71 @@
+package moran.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.dom4j.DocumentException;
+
+import moran.po.TextMessage;
+import moran.util.CheckUtil;
+import moran.util.MessageUtil;
+
+@SuppressWarnings("serial")
+public class WeixinServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String signature=req.getParameter("signature");
+		String timestamp=req.getParameter("timestamp");
+		String nonce=req.getParameter("nonce");
+		String echostr=req.getParameter("echostr");
+		
+		PrintWriter out=resp.getWriter();
+		if(CheckUtil.checkSignature(signature, timestamp, nonce)) {
+			out.print(echostr);
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		req.setCharacterEncoding("UTF-8");
+		resp.setCharacterEncoding("UTF-8");
+		PrintWriter out=resp.getWriter();
+		try {
+			 Map<String,String> map=MessageUtil.xmlToMap(req);
+			 String fromUserName=map.get("FromUserName");
+			 String toUserName=map.get("ToUserName");
+			 String msgType=map.get("MsgType");
+			 String content=map.get("Content");
+			 
+			 String message=null;
+			 if("text".equals(msgType)) {
+				 TextMessage text=new TextMessage();
+				 text.setFromUserName(toUserName);
+				 text.setToUserName(fromUserName);
+				 text.setMsgType("text");
+				 text.setCreateTime(String.valueOf(new Date().getTime()));
+				 text.setContent("您发送的消息是："+content);
+				 message=MessageUtil.textMessageToxml(text);
+				 
+				 System.out.println(message);
+			 }
+			 out.print(message);
+			 
+		 }catch(DocumentException e) {
+			 e.printStackTrace();
+		 }finally {
+			 out.close();
+		 }
+		
+	}
+
+	
+}
---
+++ Weixin/src/moran/util/MessageUtil.java
@@ -0,0 +1,45 @@
+package moran.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.io.SAXReader;
+import org.dom4j.Element;
+
+import com.thoughtworks.xstream.XStream;
+
+import moran.po.TextMessage;
+
+public class MessageUtil {
+	/*xml转换为map类型*/
+	public static Map<String,String> xmlToMap(HttpServletRequest request) throws IOException, DocumentException{
+		Map<String,String> map=new HashMap<String,String>();
+		SAXReader reader=new SAXReader();
+		
+		InputStream ins=request.getInputStream();
+		Document doc=reader.read(ins);
+		
+		Element root=doc.getRootElement();
+		
+		List<Element> list=root.elements();
+		
+		for(Element e:list) {
+			map.put(e.getName(), e.getText());
+		}
+		ins.close();
+		return map;
+	}
+	
+	/*文本消息对象转换为xml类型*/
+	public static String textMessageToxml(TextMessage textMessage) {
+		XStream xstream=new XStream();
+		xstream.alias("xml", textMessage.getClass());
+		return xstream.toXML(textMessage);
+	}
+}
